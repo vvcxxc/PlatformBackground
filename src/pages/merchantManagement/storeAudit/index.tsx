94f531d80e99432c9b9683b8ecd2c4e9eba19f32
songyizhe <769724661@qq.com>
2020-01-08 15:55:14 +0800

    a

---
+++ src/pages/merchantManagement/storeAudit/index.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import styles from './index.less'
+
+class StoreAudit extends Component {
+  state = {
+
+  }
+
+  render (){
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+export default StoreAudit
