5e9132e162f8c04d4d2929e9745dbafdcbd99027
songyizhe <769724661@qq.com>
2020-04-22 09:19:29 +0800

    misx

--- src/pages/communityManagement/community-list/index.tsx
+++ src/pages/communityManagement/community-list/index.tsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import styles from './index.less';
-import { Breadcrumb, Row, Col, Form, Button, Select, Input, DatePicker} from 'antd';
+import { Breadcrumb, Row, Col, Form, Button, Select, Input, DatePicker } from 'antd';
 const FormItem = Form.Item;
 const { Option } = Select;
-
+const { RangePicker, } = DatePicker;
 
 interface Props {
   form: any;
@@ -17,13 +17,20 @@ export default Form.create()(
 
     render() {
       const { getFieldDecorator } = this.props.form;
-
+      const columns = [
+        {
+          title: '提交审核时间',
+          dataIndex: 'created_at',
+          key: 'created_at',
+          width: 100
+        }
+      ]
       return (
         <div className={styles.listPage}>
-          <Breadcrumb>
+          {/* <Breadcrumb>
             <Breadcrumb.Item>社群列表</Breadcrumb.Item>
-          </Breadcrumb>
-          <Form>
+          </Breadcrumb> */}
+          <Form layout="inline">
             <Row
               gutter={{
                 md: 8,
@@ -31,25 +38,34 @@ export default Form.create()(
                 xl: 48,
               }}
             >
-               <Col md={8} sm={24}>
-                  <FormItem label='入驻时间'>
-                    <Form.Item
-                      style={{ display: 'inline-block', width: 'calc(40% - 12px)' }}
-                    >
-                      {/* <DatePicker /> */}
-                      {getFieldDecorator('start_date', { initialValue: start_date })(
-                        <DatePicker />
-                      )}
-                    </Form.Item>
-                    <span style={{ display: 'inline-block', width: '24px', textAlign: 'center' }}>-</span>
-                    <Form.Item style={{ display: 'inline-block', width: 'calc(40% - 12px)' }}>
-                      {/* <DatePicker /> */}
-                      {getFieldDecorator('end_date', { initialValue: end_date })(
-                        <DatePicker />
-                      )}
-                    </Form.Item>
-                  </FormItem>
-                </Col>
+              <Col md={8} sm={24}>
+                <FormItem label='注册时间'>
+                    <RangePicker />
+                </FormItem>
+              </Col>
+              <Col md={8} sm={24}>
+                <FormItem label='申请时间'>
+                    <RangePicker />
+                </FormItem>
+              </Col>
+              <Col md={6} sm={24}>
+                <FormItem label='手机号'>
+                    <Input />
+                </FormItem>
+              </Col>
+            </Row>
+            <Row
+              gutter={{
+                md: 8,
+                lg: 24,
+                xl: 48,
+              }}
+            >
+              <Col md={6} sm={24}>
+                <FormItem label='个人名称'>
+                    <Input />
+                </FormItem>
+              </Col>
             </Row>
           </Form>
         </div>
